0d9d37d63a458f14fba358f8bf07c32487c4cc74
syf <613526251@qq.com>
2020-09-05 21:13:26 +0800

    initial

--- eureka-regist/src/main/java/cn/com/ivan/eurekaregist/eurekaregist/EurekaRegistApplication.java
+++ eureka-regist/src/main/java/cn/com/ivan/eurekaregist/eurekaregist/EurekaRegistApplication.java
@@ -1,4 +1,4 @@
-package cn.com.ivan.eureka;
+package cn.com.ivan.eurekaregist.eurekaregist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaRegistApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(EurekaRegistApplication.class, args);
     }
 
 }
